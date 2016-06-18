6b43635c76130dc89936242335d9eedb243da994
Carlos Orellana Marroquín <carlos.cw2106@gmail.com>
2016-06-18 11:30:09 -0600

    Fixing some little issues with the index action

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -56,6 +56,6 @@ class BookmarksController < ApplicationController
     end
 
     def bookmark_params
-      params.require(:bookmark).permit(:url, :title)
+      params.require(:bookmark).permit(:url, :title, :image, :paragraph)
     end
 end
